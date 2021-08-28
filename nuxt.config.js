b4600de31193d4e03aed5e73a14d46d1764c6988
Adhish Yadav <adhishyadav@outlook.com>
2021-08-28 12:17:11 +0530

    Updated application name & title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Adhish_Yadav',
+    title: 'Truecaller Blog',
     htmlAttrs: {
       lang: 'en'
     },
